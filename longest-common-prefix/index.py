0c642679a4ec81ee157e9daf0f8757ccfc10d0f0
Abrham <abrsh6266@gmail.com>
2024-01-12 15:45:19 -0900

    longest-common-prefix

---
+++ longest-common-prefix/index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        n = len(strs)
+        minimum = float('inf')
+        ss = ''
+        for i in range(n):
+            minimum = min(minimum,len(strs[i]))
+        print(minimum)
+        for i in range(minimum):
+            cur = ss+(strs[0])[i]
+            for j in range(n):
+                if cur != strs[j][0:len(cur)]:
+                    return ss
+            ss = cur
+        return ss
+solution = Solution()
+print(solution.longestCommonPrefix(["flower","flow","flight"]))
+                
