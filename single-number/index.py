42290dd33dfa895aad2c00846968fa7791bbfc03
Abrham <abrsh6266@gmail.com>
2024-02-07 23:12:54 -0900

    single number

---
+++ single-number/index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, num: list[int]) -> int:
+        nums = sorted(num)
+        print(nums)
+        i=0
+        n = len(nums)
+        while i<n:
+            if i == 0 and n>1 and nums[i] != nums[i+1]:
+                return nums[i]
+            if i==n-1:
+                return nums[i]
+            if nums[i] == nums[i+1] or nums[i-1] == nums[i]:
+                i += 1
+            else:
+                return nums[i]
+        return nums[n-1]
+solution = Solution()
+print(solution.singleNumber([4,1,2,1,2]))
